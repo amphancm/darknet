d6f6eb4f455c54aad06c1d29a3a3958c3d10bf3a
Adun Nantakaew <amphancm@gmail.com>
2023-03-21 10:33:02 +0700

    Add files via upload

---
+++ generate_val.py
@@ -0,0 +1,20 @@
+import os
+
+image_files = []
+i = 0
+os.chdir(os.path.join("data", "val"))
+for filename in os.listdir(os.getcwd()):
+    if filename.endswith(".jpg"):
+        image_files.append("data/val/" + filename)
+        i+=1
+print('Total Val JPG file = ',i)  
+i = 0           
+os.chdir("..")
+with open("val.txt", "w") as outfile:
+    for image in image_files:
+        outfile.write(image)
+        outfile.write("\n")
+        i+=1
+    outfile.close()
+    print('Total Val Txt file = ',i) 
+os.chdir("..")
